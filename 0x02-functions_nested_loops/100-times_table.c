be431be73528698bdf5b8002a87c89d213bd0601
Attajnida <hamzaattajnida@gmail.com>
2023-06-21 18:58:36 -0700

    test 1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,40 +1,51 @@
 #include "main.h"
 
 /**
- * print_times_table - Prints the times table of the input
- * starting with 0.
- * @n: The value of the times table to be printed.
- */
-
+ * print_times_table - Prints the n times table, starting with 0.
+ * @n: The n times.
+ *
+ * Return: void.
+ **/
 void print_times_table(int n)
 {
-	int num, mult, prod;
-	if (n > 0 && n <= 15)
-	{
-	for (num = 0; num <= n; num++)
-	{
-	_putchar('0');
-	for (mult = 1; mult <= n; mult++)
-	{
-	_putchar(',');
-	_putchar(' ');
-	prod = num * mult;
-	if (prod <= 99)
-	_putchar(' ');
-	if (prod <= 9)
-	_putchar(' ');
-	if (prod >= 100)
-	{
-	_putchar((prod / 100) + '0');
-	_putchar(((prod / 10)) % 10 + '0');
-	}
-	else if (prod <= 99 && prod >= 10)
-	{
-	_putchar((prod / 10) + '0');
-	}
-	_putchar((prod % 10) + '0');
-	}
-	_putchar('\n');
-	}
-	}
+        int i = 0, j;
+
+        if (n < 0 || n > 15)
+                return;
+        while (i <= n)
+        {
+                j = 0;
+                while (j <= n)
+                {
+                        if ((i * j) < 10)
+                        {
+                                if (j != 0)
+                                {
+                                        _putchar(' ');
+                                        _putchar(' ');
+                                }
+                                _putchar((i * j) + '0');
+                        }
+                        else if ((i * j) < 100)
+                        {
+                                _putchar(' ');
+                                _putchar(((i * j) / 10) + '0');
+                                _putchar(((i * j) % 10) + '0');
+                        }
+                        else
+                        {
+                                _putchar(((i * j) / 100) + '0');
+                                _putchar((((i * j) / 10) % 10) + '0');
+                                _putchar(((i * j) % 10) + '0');
+                        }
+                        if (j != n)
+                        {
+                                _putchar(',');
+                                _putchar(' ');
+                        }
+                        j++;
+                }
+                _putchar('\n');
+                i++;
+        }
 }
