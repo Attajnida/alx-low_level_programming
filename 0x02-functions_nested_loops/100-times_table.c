71c4326691084155bd070e8c3568ec349484bd01
Attajnida <hamzaattajnida@gmail.com>
2023-06-21 19:01:41 -0700

    final test

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,44 +8,44 @@
  **/
 void print_times_table(int n)
 {
-        int i = 0, j;
+	int i = 0, j;
 
-        if (n < 0 || n > 15)
-                return;
-        while (i <= n)
-        {
-                j = 0;
-                while (j <= n)
-                {
-                        if ((i * j) < 10)
-                        {
-                                if (j != 0)
-                                {
-                                        _putchar(' ');
-                                        _putchar(' ');
-                                }
-                                _putchar((i * j) + '0');
-                        }
-                        else if ((i * j) < 100)
-                        {
-                                _putchar(' ');
-                                _putchar(((i * j) / 10) + '0');
-                                _putchar(((i * j) % 10) + '0');
-                        }
-                        else
-                        {
-                                _putchar(((i * j) / 100) + '0');
-                                _putchar((((i * j) / 10) % 10) + '0');
-                                _putchar(((i * j) % 10) + '0');
-                        }
-                        if (j != n)
-                        {
-                                _putchar(',');
-                                _putchar(' ');
-                        }
-                        j++;
-                }
-                _putchar('\n');
-                i++;
-        }
+	if (n < 0 || n > 15)
+		return;
+	while (i <= n)
+	{
+		j = 0;
+		while (j <= n)
+		{
+			if ((i * j) < 10)
+			{
+				if (j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar((i * j) + '0');
+			}
+			else if ((i * j) < 100)
+			{
+				_putchar(' ');
+				_putchar(((i * j) / 10) + '0');
+				_putchar(((i * j) % 10) + '0');
+			}
+			else
+			{
+				_putchar(((i * j) / 100) + '0');
+				_putchar((((i * j) / 10) % 10) + '0');
+				_putchar(((i * j) % 10) + '0');
+			}
+			if (j != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
